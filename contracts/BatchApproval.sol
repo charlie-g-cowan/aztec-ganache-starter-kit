d8aa1cf4085c7df2c16dfa931f0c1837798cd517
Charlie Cowan <charlie.g.cowan@gmail.com>
2019-08-08 15:04:40 +0100

    Brought ownership check out into modifier

--- contracts/BatchApproval.sol
+++ contracts/BatchApproval.sol
@@ -14,8 +14,28 @@ contract BatchApproval is Ownable, IAZTEC {
     using NoteUtils for bytes;
     address public aceAddress;
 
+    /**
+     * @notice              Constructor for this contract, simply saves the address of ACE
+     * @param _aceAddress   Address of ACE contract for use by this contract
+     */
     constructor(address _aceAddress) public Ownable() {
         aceAddress = _aceAddress;
+    }
+
+    /**
+     * @notice              Check that all notes that have been passed to the contract are owned by it
+     * @author              AZTEC
+     * @param _noteHashes   An array of hashes of notes
+     * @param _zkAsset      The address of the zkAsset that minted these notes
+     */
+    modifier notesOwned(bytes32[] memory _noteHashes, address _zkAsset) {
+        for (uint i = 0; i < _noteHashes.length; i++) {
+            // (uint8 status, , , address noteOwner) = ACE(aceAddress).getNote(_zkAsset, _noteHashes[i]);
+            // require(status == uint8(NoteStatus.UNSPENT));
+            (, , , address noteOwner) = ACE(aceAddress).getNote(_zkAsset, _noteHashes[i]);
+            require(noteOwner == address(this), "Contract does not own this note.");
+        }
+        _;
     }
 
     /**
@@ -26,13 +46,7 @@ contract BatchApproval is Ownable, IAZTEC {
      * @param _spender      The address of the person or contract that is being approved to spend these notes.
      *                      Can be any person or contract e.g. Bob, a different third-party, a contract, this contract itself.
      */
-    function batchApprove(bytes32[] memory _noteHashes, address _zkAsset, address _spender) public onlyOwner {
-        for (uint i = 0; i < _noteHashes.length; i++) {
-            // (uint8 status, , , address noteOwner) = ACE(aceAddress).getNote(_zkAsset, _noteHashes[i]);
-            // require(status == uint8(NoteStatus.UNSPENT));
-            (, , , address noteOwner) = ACE(aceAddress).getNote(_zkAsset, _noteHashes[i]);
-            require(noteOwner == address(this), "Contract does not own this note.");
-        }
+    function batchApprove(bytes32[] memory _noteHashes, address _zkAsset, address _spender) public onlyOwner notesOwned(_noteHashes, _zkAsset) {
         IZkAsset asset = IZkAsset(_zkAsset);
         for (uint j = 0; j < _noteHashes.length; j++) {
             asset.confidentialApprove(_noteHashes[j], _spender, true, '');
