6a2738f3c5a73ff203fbc769db06cccbbcabc490
Charlie Cowan <charlie.g.cowan@gmail.com>
2019-08-09 17:29:17 +0100

    Added spending ability and tests

--- contracts/BatchApproval.sol
+++ contracts/BatchApproval.sol
@@ -7,26 +7,26 @@ import "@aztec/protocol/contracts/interfaces/IAZTEC.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
- * @title   Contract for approving a set of notes to be spent by another party. Simplifies flow for user by only needing to approve one transaction.
- * @author  AZTEC
+ * @title Contract for approving a set of notes to be spent by another party. Simplifies flow for user by only needing to approve one transaction.
+ * @author AZTEC
  */
 contract BatchApproval is Ownable, IAZTEC {
     using NoteUtils for bytes;
     address public aceAddress;
 
     /**
-     * @notice              Constructor for this contract, simply saves the address of ACE
-     * @param _aceAddress   Address of ACE contract for use by this contract
+     * @notice Constructor for this contract, simply saves the address of ACE
+     * @param _aceAddress Address of ACE contract for use by this contract
      */
     constructor(address _aceAddress) public Ownable() {
         aceAddress = _aceAddress;
     }
 
     /**
-     * @notice              Check that all notes that have been passed to the contract are owned by it
-     * @author              AZTEC
-     * @param _noteHashes   An array of hashes of notes
-     * @param _zkAsset      The address of the zkAsset that minted these notes
+     * @notice Check that all notes that have been passed to the contract are owned by it
+     * @author AZTEC
+     * @param _noteHashes An array of hashes of notes
+     * @param _zkAsset The address of the zkAsset that minted these notes
      */
     modifier notesOwned(bytes32[] memory _noteHashes, address _zkAsset) {
         for (uint i = 0; i < _noteHashes.length; i++) {
@@ -39,17 +39,29 @@ contract BatchApproval is Ownable, IAZTEC {
     }
 
     /**
-     * @notice              Allows user who owns this contract to approve a set of notes owned by this contract for spending by a party
-     * @author              AZTEC
-     * @param _noteHashes   An array of hashes of notes (that must be owned by this contract) to to be approved for spending
-     * @param _zkAsset      The address of the zkAsset that minted these notes
-     * @param _spender      The address of the person or contract that is being approved to spend these notes.
-     *                      Can be any person or contract e.g. Bob, a different third-party, a contract, this contract itself.
+     * @notice Allows user who owns this contract to approve a set of notes owned by this contract for spending by a party
+     * @author AZTEC
+     * @param _noteHashes An array of hashes of notes (that must be owned by this contract) to to be approved for spending
+     * @param _zkAsset The address of the zkAsset that minted these notes
+     * @param _spender The address of the person or contract that is being approved to spend these notes.
+     *                 Can be any person or contract e.g. Bob, a different third-party, a contract, this contract itself.
      */
     function batchApprove(bytes32[] memory _noteHashes, address _zkAsset, address _spender) public onlyOwner notesOwned(_noteHashes, _zkAsset) {
         IZkAsset asset = IZkAsset(_zkAsset);
         for (uint j = 0; j < _noteHashes.length; j++) {
             asset.confidentialApprove(_noteHashes[j], _spender, true, '');
         }
+    }
+
+    /**
+     * @notice Validates a Join-Split proof to transfer notes to another addresstwo
+     * @author AZTEC
+     * @param _proof The proof data to verify
+     * @param _zkAsset The address of the zkAsset
+     */
+    function proofValidation(bytes memory _proof, address _zkAsset, address _sender) public onlyOwner {
+        IZkAsset asset = IZkAsset(_zkAsset);
+        (bytes memory _proofOutputs) = ACE(aceAddress).validateProof(JOIN_SPLIT_PROOF, _sender, _proof);
+        asset.confidentialTransferFrom(JOIN_SPLIT_PROOF, _proofOutputs.get(0));
     }
 }
--- test/BatchApproval.js
+++ test/BatchApproval.js
@@ -37,22 +37,27 @@ contract('BatchApproval', async (accounts) => {
         ace = await ACE.at(ACE.address);
     });
 
-    let mintedNotes;
+    let approvedMintedNotes;
+    let nonApprovedMintedNotes;
 
     it('owner of the contract should be able to mint notes that are owned by the contract', async () => {
-        mintedNotes = [];
-        mintedNotes[0] = await aztec.note.create(alice.publicKey, 50, batchApprovalContract.address);
-        mintedNotes[1] = await aztec.note.create(alice.publicKey, 75, batchApprovalContract.address);
-        mintedNotes[2] = await aztec.note.create(alice.publicKey, 100, batchApprovalContract.address);
+        approvedMintedNotes = [];
+        approvedMintedNotes[0] = await aztec.note.create(alice.publicKey, 50, batchApprovalContract.address);
+        approvedMintedNotes[1] = await aztec.note.create(alice.publicKey, 75, batchApprovalContract.address);
+        approvedMintedNotes[2] = await aztec.note.create(alice.publicKey, 100, batchApprovalContract.address);
+        nonApprovedMintedNotes = [];
+        nonApprovedMintedNotes[0] = await aztec.note.create(alice.publicKey, 25, batchApprovalContract.address);
+        nonApprovedMintedNotes[1] = await aztec.note.create(alice.publicKey, 125, batchApprovalContract.address);
+        let allNotes = approvedMintedNotes.concat(nonApprovedMintedNotes);
 
-        const newMintCounterNote = await aztec.note.create(alice.publicKey, 225);
+        const newMintCounterNote = await aztec.note.create(alice.publicKey, 375);
         const zeroMintCounterNote = await aztec.note.createZeroValueNote();
         const sender = zkAssetMintableContract.address;
 
         const mintProof = new MintProof(
             zeroMintCounterNote,
             newMintCounterNote,
-            mintedNotes,
+            allNotes,
             sender,
         );
 
@@ -60,16 +65,85 @@ contract('BatchApproval', async (accounts) => {
 
 
         await zkAssetMintableContract.confidentialMint(MINT_PROOF, mintData, {from: alice.address});
-        expect(await ace.getNote(zkAssetMintableContract.address, mintedNotes[0].noteHash)).to.not.equal(undefined);
+        expect(await ace.getNote(zkAssetMintableContract.address, approvedMintedNotes[0].noteHash)).to.not.equal(undefined);
+        expect(await ace.getNote(zkAssetMintableContract.address, approvedMintedNotes[1].noteHash)).to.not.equal(undefined);
+        expect(await ace.getNote(zkAssetMintableContract.address, approvedMintedNotes[2].noteHash)).to.not.equal(undefined);
+        expect(await ace.getNote(zkAssetMintableContract.address, nonApprovedMintedNotes[0].noteHash)).to.not.equal(undefined);
+        expect(await ace.getNote(zkAssetMintableContract.address, nonApprovedMintedNotes[1].noteHash)).to.not.equal(undefined);
     });
 
     it('owner of the contract should be able to approve notes that are owned by the contract to be spent', async () => {
-        const noteHashes = mintedNotes.map(note => note.noteHash);
+        const noteHashes = approvedMintedNotes.map(note => note.noteHash);
         await batchApprovalContract.batchApprove(noteHashes, zkAssetMintableContract.address, batchApprovalContract.address);
 
-        let note0Approved = await zkAssetMintableContract.confidentialApproved(mintedNotes[0].noteHash, batchApprovalContract.address);
-        let note1Approved = await zkAssetMintableContract.confidentialApproved(mintedNotes[1].noteHash, batchApprovalContract.address);
-        let note2Approved = await zkAssetMintableContract.confidentialApproved(mintedNotes[2].noteHash, batchApprovalContract.address);
-        expect(note0Approved && note1Approved && note2Approved).to.equal(true);
+        let note0Approved = await zkAssetMintableContract.confidentialApproved(approvedMintedNotes[0].noteHash, batchApprovalContract.address);
+        let note1Approved = await zkAssetMintableContract.confidentialApproved(approvedMintedNotes[1].noteHash, batchApprovalContract.address);
+        let note2Approved = await zkAssetMintableContract.confidentialApproved(approvedMintedNotes[2].noteHash, batchApprovalContract.address);
+        let note0nonApproved = await zkAssetMintableContract.confidentialApproved(nonApprovedMintedNotes[0].noteHash, batchApprovalContract.address);
+        let note1nonApproved = await zkAssetMintableContract.confidentialApproved(nonApprovedMintedNotes[1].noteHash, batchApprovalContract.address);
+        expect(note0Approved && note1Approved && note2Approved && !note0nonApproved && !note1nonApproved).to.equal(true);
     });
+
+    it('the contract should be able to spend notes after they have been approved for it to spend', async () => {
+        const invoice = await aztec.note.create(bob.publicKey, 100);
+        const change = await aztec.note.create(alice.publicKey, 125, batchApprovalContract.address);
+        
+        const sendProof = new JoinSplitProof(
+            approvedMintedNotes,
+            [invoice, change],
+            batchApprovalContract.address,
+            0,
+            batchApprovalContract.address,
+        );
+
+        const sendProofData = sendProof.encodeABI(zkAssetMintableContract.address);
+        let result = await batchApprovalContract.proofValidation(sendProofData, zkAssetMintableContract.address, batchApprovalContract.address);
+        expect(result.receipt.status).to.equal(true);
+    });
+
+    it('the contract shouldn\'t be able to spend unapproved notes', async() => {
+        const invoice = await aztec.note.create(bob.publicKey, 100);
+        const change = await aztec.note.create(alice.publicKey, 50, batchApprovalContract.address);
+        const sendProof = new JoinSplitProof(
+            nonApprovedMintedNotes,
+            [invoice, change],
+            batchApprovalContract.address,
+            0,
+            batchApprovalContract.address,
+        );
+
+        const sendProofData = sendProof.encodeABI(zkAssetMintableContract.address);
+        try {
+            await batchApprovalContract.proofValidation(sendProofData, zkAssetMintableContract.address, batchApprovalContract.address);
+            throw new Error('JoinSplit succeeds but notes are not approved');
+        } catch (err) {
+            if (err.reason !== 'sender does not have approval to spend input note') {
+                throw err;
+            }
+        }
+    });
+
+    it('the contract shouldn\'t be able to spend notes that it has already spent', async () => {
+        const invoice = await aztec.note.create(bob.publicKey, 100);
+        const change = await aztec.note.create(alice.publicKey, 125, batchApprovalContract.address);
+        
+        const sendProof = new JoinSplitProof(
+            approvedMintedNotes,
+            [invoice, change],
+            batchApprovalContract.address,
+            0,
+            batchApprovalContract.address,
+        );
+
+        const sendProofData = sendProof.encodeABI(zkAssetMintableContract.address);
+        try {
+            await batchApprovalContract.proofValidation(sendProofData, zkAssetMintableContract.address, batchApprovalContract.address);
+            throw new Error('JoinSplit succeeds but notes should already be spent')
+        } catch (err) {
+            if (err.reason !== 'input note status is not UNSPENT') {
+                throw err;
+            }
+        }
+    });
+
 });
